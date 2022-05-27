547285420cd92d105ab4f44b151a839986a0f23d
Egor Suvorov <egor_suvorov@mail.ru>
2022-05-27 16:41:07 +0300

    logitech-camera-settings: update EULA and installation URL

--- logitech-camera-settings/tools/chocolateyinstall.ps1
+++ logitech-camera-settings/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download01.logi.com/web/ftp/pub/techsupport/cameras/Webcams/LogiCameraSettings_2.12.8.exe'
+$url        = 'https://download01.logi.com/web/ftp/pub/techsupport/cameras/Webcams/LogiCameraSettings_2.12.20.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName  = 'Logitech Camera Settings'
 
-  checksum      = '4a18682e139a4bf665f7bf348c2887e52c656c1c4b3797817a2849668808bb97'
+  checksum      = '7a54eadbaf6b27e182e5cab27b62241d6be3bd99fd4b387cc38e9d700709f5c9' 
   checksumType  = 'sha256'
 
   silentArgs   = '/S'
